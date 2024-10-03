b7e856be807215ad39c5c630ed5af838e965026f
truonglm <truonglm@arrowhitech.co>
2024-10-03 11:31:23 +0700

    test

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,41 +1,5 @@
 import HeroSection from "./components/HeroSection";
 
-/** Mock data */
-const DATA_AI = [
-  {
-    title: "Customer Support",
-    description:
-      "A POS system that allows customers to shop their way, from online to checkout line.",
-  },
-  {
-    title: "Work Efficiency",
-    description:
-      "A POS system that allows customers to shop their way, from online to checkout line.",
-  },
-  {
-    title: "Report Leadership",
-    description:
-      "A POS system that allows customers to shop their way, from online to checkout line.",
-  },
-];
-/** Mock data */
-const DATA_PRIVACY = [
-  {
-    title: "Reach new leads",
-    description:
-      "A POS system that allows customers to shop their way, from online to checkout line.",
-  },
-  {
-    title: "Reach new leads",
-    description:
-      "A POS system that allows customers to shop their way, from online to checkout line.",
-  },
-  {
-    title: "Reach new leads",
-    description:
-      "A POS system that allows customers to shop their way, from online to checkout line.",
-  },
-];
 function HomeScreen() {
   return (
     <div className="flex flex-col justify-center w-full px-3 sm:px-6 md:px-20 bg-white gap-y-2.5 sm:gap-y-10">
